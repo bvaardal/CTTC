eb9c11b44c8f61400a8a65f095e73dabc2a4c221
melzayat <melzayat@ca.ibm.com>
2013-11-19 01:14:29 -0500

    added logo, Actionbar, search button , store locator , etc , plus some UI enhancements

--- eclipseAndroidADT2Workspace/HellowWorld/src/com/example/hellowworld/MainActivity.java
+++ eclipseAndroidADT2Workspace/HellowWorld/src/com/example/hellowworld/MainActivity.java
@@ -10,7 +10,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.Toast;
@@ -24,6 +27,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
         setContentView(R.layout.activity_main);
         
         createGroupList();
@@ -116,9 +121,25 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+       MenuInflater inflater = getMenuInflater();
+       inflater.inflate(R.menu.main_activity_actions, menu);
+       return super.onCreateOptionsMenu(menu);
     }
+    
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle presses on the action bar items
+//        switch (item.getItemId()) {
+//            case R.id.action_search:
+//                openSearch();
+//                return true;
+//            case R.id.action_settings:
+//                openSettings();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
     
 //    public void displayMessage(View view){
 //    	Intent intent = new Intent(this,DisplayMessageActivity.class);
